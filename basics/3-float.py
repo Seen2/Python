c919d2700e4a073153e98e469728d2c24b2b801c
seen2 <tiwarietc@gmail.com>
2019-02-16 20:49:38 +0530

    floating type print

---
+++ basics/3-float.py
@@ -0,0 +1,10 @@
+def main():
+    x = float(input("Enter x:"))
+    y = float(input("Enter y:"))
+
+    print(f"x times y={(x*y):.2f}")
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ basics/4-percision.py
@@ -0,0 +1,11 @@
+def main():
+    x = float(input("Enter x:"))
+    y = float(input("Enter y:"))
+
+# floating point precision
+    print(f"x times y={(x*y):.50f}")
+
+
+if __name__ == '__main__':
+    main()
+
