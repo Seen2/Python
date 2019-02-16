6416a271c463eeb8423695c198d18589d51d2c6c
seen2 <tiwarietc@gmail.com>
2019-02-16 20:20:57 +0530

    basic control flow and arthemtic

---
+++ basics/1-athematic.py
@@ -0,0 +1,16 @@
+def main():
+    x=int(input("Enter x:"))
+    y=int(input("Enter y:"))
+
+    print(f"x+y={x + y}")
+    print(f"x-y= {x - y}")
+    print(f"x*y={x * y}")
+    print(f"x/y={x / y}")
+    print(f"x//y={x // y}")
+    print(f"x%y={x % y}")
+    print(f"x**y={x ** y}")
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ basics/2-conditional.py
@@ -0,0 +1,14 @@
+def main():
+    x=int(input("Enter x:"))
+    y=int(input("Enter y:"))
+
+    if x > y:
+        print("x is greater than y")
+    elif x < y:
+        print("x is less than y")
+    else:
+        print("x is equal to y")
+
+
+if __name__ == '__main__':
+    main()
