3ffe36183806f8ea5167b5e3de1001491b5afdc2
seen2 <tiwarietc@gmail.com>
2019-02-17 20:09:45 +0530

    comptetive switch case

---
+++ basics/9-switch_case.py
@@ -0,0 +1,17 @@
+def main():
+	x = int(input("Enter \n 0 for programing \n 1 for love "))
+
+	if x is 0:
+		print("You'll have no life to cheers or You'll enjoy it all.")
+
+	if x is 1:
+		print("This is virus, stay away from this.")
+
+	else:
+		print("you are not infected.")
+
+
+if __name__ == '__main__':
+	main()
+
+
