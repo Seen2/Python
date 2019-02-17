a4046ee087abbf0866c3d2f35df845baeca84619
seen2 <tiwarietc@gmail.com>
2019-02-17 20:09:19 +0530

    relational stuffs

---
+++ basics/8-relational.py
@@ -0,0 +1,15 @@
+def main():
+	x = input("x=")
+
+	if x == 'Y' or x == 'y':
+		print("Yes..")
+	elif x == 'n' or x == 'N':
+		print("No...")
+	else:
+		print("Well, try again...")
+
+
+if __name__ == '__main__':
+	main()
+
+
