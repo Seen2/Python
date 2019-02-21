9bfda9bd9195d6325818605a3ed35600a34a6629
seen2 <tiwarietc@gmail.com>
2019-02-21 23:53:29 +0530

    string built in methods

--- StringHandling/main.py
+++ StringHandling/main.py
@@ -1,5 +1,16 @@
+# builtIn ,methods
+# s.capitalize(    s.format(        s.isidentifier(  s.ljust(         s.rjust(         s.swapcase(
+# s.casefold(      s.format_map(    s.islower(       s.lower(         s.rpartition(    s.title(
+# s.center(        s.index(         s.isnumeric(     s.lstrip(        s.rsplit(        s.translate(
+# s.count(         s.isalnum(       s.isprintable(   s.maketrans(     s.rstrip(        s.upper(
+# s.encode(        s.isalpha(       s.isspace(       s.partition(     s.split(         s.zfill(
+# s.endswith(      s.isascii(       s.istitle(       s.replace(       s.splitlines(
+# s.expandtabs(    s.isdecimal(     s.isupper(       s.rfind(         s.startswith(
+# s.find(          s.isdigit(       s.join(          s.rindex(        s.strip(
+
+
 def main():
-    s = "hello, world!"
+    s = "Shintu Tiwari is Broke world!"
 
     print(any(s))
 
