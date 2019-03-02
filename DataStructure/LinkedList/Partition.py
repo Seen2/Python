6adab3a34b05dbd8c77f6a5740ced9be5012185e
seen2 <tiwarietc@gmail.com>
2019-03-02 12:11:53 +0530

    solved partition

---
+++ DataStructure/LinkedList/Partition.py
@@ -0,0 +1,38 @@
+from linkedList import LinkedList
+
+
+def partition(l, x):
+    ptr = l
+    lt = []
+    gt = []
+    while ptr != None:
+        if ptr.data >= x:
+            gt.append(ptr.data)
+        else:
+            lt.append(ptr.data)
+        ptr = ptr.next
+    l = lt+gt
+    lList = LinkedList(l[0])
+    n = len(l)
+    for i in range(1, n):
+        lList.insert(l[i])
+    del l, gt, lt, ptr
+    return lList
+
+
+def main():
+    l = LinkedList(3)
+    l.insert(5)
+    l.insert(8)
+    l.insert(5)
+    l.insert(10)
+    l.insert(2)
+    l.insert(1)
+    l.display()
+    l = partition(l, 5)
+    print("next")
+    l.display()
+
+
+if __name__ == "__main__":
+    main()
